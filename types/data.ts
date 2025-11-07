f6a64d403f62aa35340d4dc1ddaf1a94f166297e
ErickDsBR <erick.brasil2102@gmail.com>
2025-11-07 10:41:10 -0300

    Fist commit

---
+++ types/data.ts
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export const [product, setproduct] = useState(
+    [{
+        id: 1,
+        name: "Sample Product",
+        price: 9.99
+    }]
+);
---
+++ types/data_types.ts
@@ -0,0 +1,5 @@
+export interface newProduct {
+    id: number;
+    name: string;
+    price: number;
+};
