1a811d25504ad57c7fb9a3aeb995130ec5e7c490
ErickDsBR <erick.brasil2102@gmail.com>
2025-11-07 11:03:23 -0300

    testmain

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,15 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "../styles/auth.styles";
-import { newProduct } from "../types/data_types";
 
 export default function Index() {
-  
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        
+
         <View style={styles.container}>
           <Text>Edit app/index.tsx to edit this screen.</Text>
-          <Link href="/(tabs)/home">Go to Login</Link>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
