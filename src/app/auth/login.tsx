010063ae99da37b7022f40499b27d65029c4d329
ErickDsBR <erick.brasil2102@gmail.com>
2025-11-15 12:28:16 -0300

    re ajuste

--- src/app/auth/login.tsx
+++ src/app/auth/login.tsx
@@ -1,20 +1,15 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 // props
 import { TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 // outras pastas
 import { styles } from "../../../styles/auth.styles";
-import { userdata } from "../../../types/data_types";
 
 
 export default function login() {
     const [setemail, authmail] = useState("");
     const [setpassword, authpass] = useState("");
 
-    const handleLogin = () => {
-        // Lógica de autenticação aqui
-        userdata.
-    }
 
     return (
         <SafeAreaProvider>
@@ -23,7 +18,7 @@ export default function login() {
                     <View style={styles.boxLogin}>
                         <TextInput onChangeText={authmail} value={setemail} placeholder="Email" style={styles.authlogin} />
                         <TextInput onChangeText={authpass} value={setpassword} placeholder="Password" style={styles.authlogin} secureTextEntry={true} />
-                        <TouchableOpacity onPress={} style={styles.buttonlink}>Singup</TouchableOpacity>
+                        <TouchableOpacity style={styles.buttonlink}>Singup</TouchableOpacity>
                     </View>
                 </View>
             </SafeAreaView>
