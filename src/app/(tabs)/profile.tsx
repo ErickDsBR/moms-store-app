2acbb341df7f8ae2eca2c8abf75a0e71e8eb654f
ErickDsBR <erick.brasil2102@gmail.com>
2025-12-16 13:31:52 -0300

    vamooo

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,11 +1,30 @@
+import { supabase } from "@/lib/supabase";
 import { styles } from "@/styles/auth.styles";
-import React from "react";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, Modal, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
-const profile_functions = [];
-
 export default function profile() {
+
+  const [ingredients, setIngredients] = useState(false);
+  const [ingredientList, setIngredientList] = useState<any[]>([]);
+
+  async function fetchIngredients() {
+    const { error, data } = await supabase
+    .from("ingredientes")
+    .select("*");
+
+    if (error) {
+      console.log("Error fetching ingredients:", error);
+    }
+    else {
+      setIngredientList(data || []);
+    }
+  }
+  useEffect(() => {
+    fetchIngredients();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
@@ -14,12 +33,30 @@ export default function profile() {
           <Text style={styles.profile_name}>NAME</Text>
         </View>
         <View style={styles.profile_options}>
-          <TouchableOpacity style={styles.buttons}>
+          <TouchableOpacity style={styles.buttons} onPress={() => setIngredients(true)}>
             <Text style = {styles.button_text} >Ingredientes</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.buttons}></TouchableOpacity>
           <TouchableOpacity style={styles.buttons}></TouchableOpacity>
         </View>
+      </SafeAreaView>
+      <SafeAreaView>
+        <Modal 
+        visible={ingredients}
+        animationType="slide"
+        onRequestClose={() => setIngredients(false)}
+        >
+          <View>
+            {ingredientList.map((ingredient) => (
+              <View key={ingredient.id}>
+                <Text>{ingredient.name}</Text>
+              </View>
+            
+            ))}
+            
+          </View>
+
+        </Modal>
       </SafeAreaView>
     </SafeAreaProvider>
   );
